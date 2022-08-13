9816f745847881b183ff74ed3e74e78ee6a8b998
Mizero-eloi <80544579+Mizero-eloi@users.noreply.github.com>
2022-08-13 12:00:13 +0200

    google login

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { GoogleLogin, GoogleLogout } from "react-google-login";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import Logo from "../utils/tiktik-logo.png";
 
 const Navbar = () => {
+  const user = false;
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2  px-4">
       <Link href="/">
@@ -18,6 +19,19 @@ const Navbar = () => {
           />
         </div>
       </Link>
+
+      <div>SEARCH</div>
+
+      <div>
+        {user ? (
+          <div>Logged In</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => console.log(response)}
+            onError={() => console.log("Error")}
+          />
+        )}
+      </div>
     </div>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { useEffect, useState } from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import Navbar from "./../components/Navbar";
 import Sidebar from "./../components/Sidebar";
 
@@ -14,7 +15,9 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   if (isSSR) return null;
 
   return (
-    <div>
+    <GoogleOAuthProvider
+      clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}
+    >
       <Navbar />
       <div className="flex gap-6 md:gap-20">
         {/* sidebar  */}
@@ -26,7 +29,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           <Component {...pageProps} />
         </div>
       </div>
-    </div>
+    </GoogleOAuthProvider>
   );
 };
 
