e6d4bec5e290081dde5e89c68430f954194128f1
Mizero-eloi <80544579+Mizero-eloi@users.noreply.github.com>
2022-08-13 13:54:41 +0200

    authetication

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,9 +5,13 @@ import { useRouter } from "next/router";
 import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import Logo from "../utils/tiktik-logo.png";
 import { createOrGetUser } from "../utils";
+import useAuthStore from "../store/authStore";
+import { IoMdAdd } from "react-icons/io";
+import { AiOutlineLogout } from "react-icons/ai";
 
 const Navbar = () => {
-  const user = false;
+  const { userProfile, addUser, removeUser } = useAuthStore();
+  console.log("UserProfile", userProfile);
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2  px-4">
       <Link href="/">
@@ -24,11 +28,41 @@ const Navbar = () => {
       <div>SEARCH</div>
 
       <div>
-        {user ? (
-          <div>Logged In</div>
+        {userProfile ? (
+          <div className="flex gap-5 md:gap-10">
+            <Link href="/upload">
+              <button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
+                <IoMdAdd className="text-xl" /> {` `}
+                <span className="hidden md:block">Upload</span>
+              </button>
+            </Link>
+            {userProfile.image && (
+              <Link href="/">
+                <>
+                  <Image
+                    width={40}
+                    height={40}
+                    className="rounded-full object-cover"
+                    src={userProfile.image}
+                    alt="profile picture"
+                  />
+                </>
+              </Link>
+            )}
+            <button
+              type="button"
+              className="px-2"
+              onClick={() => {
+                googleLogout();
+                removeUser();
+              }}
+            >
+              <AiOutlineLogout color="red" fontSize={21} />
+            </button>
+          </div>
         ) : (
           <GoogleLogin
-            onSuccess={(response) => createOrGetUser(response)}
+            onSuccess={(response) => createOrGetUser(response, addUser)}
             onError={() => console.log("Error")}
           />
         )}
--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ import jwt_decode from "jwt-decode";
 
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-export const createOrGetUser = async (response: any) => {
+export const createOrGetUser = async (response: any, addUser: any) => {
   const decode: { name: string; picture: string; sub: string } = jwt_decode(
     response.credential
   );
@@ -17,7 +17,7 @@ export const createOrGetUser = async (response: any) => {
     image: picture,
   };
 
-  // addUser(user);
+  addUser(user);
 
   await axios.post("http://localhost:3000/api/auth", user);
 };
